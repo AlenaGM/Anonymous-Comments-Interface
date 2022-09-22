70e4fe3a87a51b43b202f39c105d8bc81793fe38
AlenaGM <alena.guillaume4@gmail.com>
2022-09-22 14:39:35 +0200

    upd css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,104 @@
-let storedArray = []
+let storedArray = [];
 
-// ЗАБИРАЕМ ДАННЫЕ ИЗ ФОРМЫ
+// COLLECTING FORM-DATA
 
-document.getElementById('button').onclick = (e) =>{
+document.getElementById("button").onclick = (e) => {
+  e.preventDefault();
+  //collect inputs' values
+  const author = document.getElementById("author").value;
+  const comment = document.getElementById("comment").value;
+  const photo = document.getElementById("photo").src;
 
-    e.preventDefault()
-    //собираем введенные в форму значения
-    const author = document.getElementById("author").value;
-    const comment = document.getElementById("comment").value;
-    const photo = document.getElementById('photo').src;
+  let date = new Date();
 
-    let date = new Date();
+  //format date
+  date = `${new Intl.DateTimeFormat("fr-FR", {
+    day: "2-digit",
+    month: "2-digit",
+  }).format(date)} at ${new Intl.DateTimeFormat("fr-FR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(date)}`;
 
-    //Форматироание даты
-    date = `${new Intl.DateTimeFormat('ru-RU', {day:'2-digit', month:'2-digit'}).format(date)} в ${new Intl.DateTimeFormat('ru-RU', {hour:'2-digit', minute:'2-digit'}).format(date)}`;
+  if (author && photo && comment) {
+    //generate a chat-message
+    const newCard = generateCard(author, date, photo, comment);
+    document.querySelector("#chat").appendChild(newCard);
 
-    if(author && photo && comment){
-        // Генерируем карточку и добавляем ее на страницу
-        const newCard = generateCard(author, date, photo, comment)
-        document.querySelector('#chat').appendChild(newCard)
+    //add to local storage
+    addElementToLocalStorage(author, date, photo, comment);
 
-        //Добавляем в хранилище
-        addElementToLocalStorage(author, date, photo, comment)
+    //clear comment form
+    document.getElementById("comment").value = "";
+  }
+};
 
-        //Очищаем коммент
-        document.getElementById("comment").value = '';
-        }
+//Add photo, listen to changes in <input type="file">
+document.getElementById("photo").addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onload = (function (file) {
+    return function (e) {
+      const r = e.target;
+      const photo = r.result;
+      document.getElementById("photo").src = photo;
     };
+  })(file);
 
-//Вставка аватарки, слушаем изменение в поле <input type="file">
-document.getElementById('photo').addEventListener('change', (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    // при успешном завершении операции чтения
-    reader.onload = (function (file) {
-        return function (e) {
-            const r = e.target;
-            // получаем содержимое файла, состояние чтения, ошибки(или null)
-            const photo = r.result
+  reader.readAsDataURL(file);
+});
 
-            document.getElementById('photo').src = photo
-        };
-    })(file);
+//GENERATE CARD = CHAT-MESSAGE
+const generateCard = (author, date, photo, comment) => {
+  let card = document.createElement("div");
+  card.classList.add("card");
 
-    reader.readAsDataURL(file);
-})
+  let card__image = document.createElement("img");
+  card__image.classList.add("card__image");
+  card__image.src = photo;
 
-//ГЕНЕРИРУЕМ КАРТОЧКУ = СООБЩЕНИЕ НА ФОРУМЕ
-const generateCard = (author, date, photo, comment) =>{
+  let card__main = document.createElement("div");
+  card__main.classList.add("card__main");
 
-    //Рисуем карточку
-    let card = document.createElement('div')
-    card.classList.add("card");
+  let card__info = document.createElement("div");
+  card__info.classList.add("card__info");
 
-    let card__image = document.createElement('img')
-    card__image.classList.add("card__image");
-    card__image.src = photo
+  let card__title = document.createElement("h3");
+  card__title.classList.add("card__title");
+  card__title.innerText = author;
 
-    let card__main = document.createElement('div')
-    card__main.classList.add("card__main")
+  let card__date = document.createElement("div");
+  card__date.classList.add("card__date");
+  card__date.innerText = date;
 
-    let card__info = document.createElement('div')
-    card__info.classList.add("card__info")
+  let card__text = document.createElement("p");
+  card__text.classList.add("card__text");
+  card__text.innerText = comment;
 
-    let card__title = document.createElement('h3')
-    card__title.classList.add("card__title")
-    card__title.innerText = author
+  let card__del = document.createElement("button");
+  card__del.classList.add("card__del");
+  card__del.innerHTML = "Удалить";
 
-    let card__date = document.createElement('div')
-    card__date.classList.add("card__date")
-    card__date.innerText = date
+  card.appendChild(card__image);
+  card.appendChild(card__main);
+  card.appendChild(card__del);
 
-    let card__text = document.createElement('p')
-    card__text.classList.add("card__text")
-    card__text.innerText = comment
+  card__main.appendChild(card__info);
+  card__main.appendChild(card__text);
+  card__main.appendChild(card__del);
 
-    let card__del = document.createElement('button')
-    card__del.classList.add("card__del");
-    card__del.innerHTML="Удалить"
+  card__info.appendChild(card__title);
+  card__info.appendChild(card__date);
 
-    card.appendChild(card__image)
-    card.appendChild(card__main)
+  return card;
+};
 
-    card__main.appendChild(card__info)
-    card__main.appendChild(card__text)
-    card__main.appendChild(card__del)
+//WORK WITH LOCALSTORAGE
 
-    card__info.appendChild(card__title)
-    card__info.appendChild(card__date)
+getArrFromLocalStorage = () => {
+  let collection = JSON.parse(localStorage.getItem("chatMsgsCollection"));
 
-    return card
-}
-
-//РАБОТА С LOCALSTORAGE
-
-getArrFromLocalStorage = () =>{
-
-let collection = JSON.parse(localStorage.getItem("chatMsgsCollection"));
-
-    if(collection){
+  if (collection) {
     storedArray = collection;
 
     const lastItem = collection[collection.length - 1];
@@ -106,58 +106,63 @@ let collection = JSON.parse(localStorage.getItem("chatMsgsCollection"));
     const lastPhoto = lastItem[2];
 
     document.getElementById("author").value = lastAuthor;
-    document.getElementById('photo').src = lastPhoto;
+    document.getElementById("photo").src = lastPhoto;
 
     //document.getElementById("photoPreview").innerHTML = `<span><img src="${lastPhoto}" alt="avatar" id="avatar" width="60"></span>`;
-    }
-}
+  }
+};
 
-setArrToLocalStorage = () =>{
-    localStorage.setItem("chatMsgsCollection", JSON.stringify(storedArray));
-}
+setArrToLocalStorage = () => {
+  localStorage.setItem("chatMsgsCollection", JSON.stringify(storedArray));
+};
 
 addElementToLocalStorage = (author, date, photo, comment) => {
-    storedArray.push([author, date, photo, comment])
-    setArrToLocalStorage()
-}
+  storedArray.push([author, date, photo, comment]);
+  setArrToLocalStorage();
+};
 
 //Шаг 4: Нужно получать коллекцию из хранилица при загрузке страницы
-document.addEventListener("DOMContentLoaded",function(){
-    getComments()
-})
-
-function getComments() {
-    //Находим список всех комментариев
-    getArrFromLocalStorage()
-
-    for( let i = 0; i < storedArray.length; i++){
-        //Храним комментарии в таком виде:
-        // [
-        //     [author, date, photo, comment],
-        //     [author, date, photo, comment],
-        //     ....
-        // ]
-
-        const newCard = generateCard(storedArray[i][0],storedArray[i][1], storedArray[i][2], storedArray[i][3] )
-        document.querySelector('#chat').appendChild(newCard)
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function (){//Что мы видим при загрузке
-    getArrFromLocalStorage();
-})
-
-//Удаление карточки/не работает
-document.addEventListener("click", function (ev){
-    if (ev.target.tagName === "BUTTON") {
-        let div = ev.target.parentNode;
-        div.remove();
-        //localStorage.removeItem('chatMsgsCollection'); //удаляет все :(
-        console.log('hihihi');
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  getComments();
 });
 
+function getComments() {
+  //Находим список всех комментариев
+  getArrFromLocalStorage();
 
+  for (let i = 0; i < storedArray.length; i++) {
+    //Храним комментарии в таком виде:
+    // [
+    //     [author, date, photo, comment],
+    //     [author, date, photo, comment],
+    //     ....
+    // ]
+
+    const newCard = generateCard(
+      storedArray[i][0],
+      storedArray[i][1],
+      storedArray[i][2],
+      storedArray[i][3]
+    );
+    document.querySelector("#chat").appendChild(newCard);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  //Что мы видим при загрузке
+  getArrFromLocalStorage();
+});
+
+//Удаление карточки/не работает
+document.addEventListener("click", function (ev) {
+  if (ev.target.tagName === "BUTTON") {
+    let div = ev.target.parentNode;
+    let pic = div.parentNode;
+    pic.remove();
+
+    localStorage.removeItem("chatMsgsCollection"); //удаляет все :(
+  }
+});
 
 /*
 СТАРЫЙ КОД
@@ -194,7 +199,3 @@ function generateComments(){
 document.addEventListener('DOMContentLoaded', function() {
 generateComments();
 });*/
-
-
-
-
